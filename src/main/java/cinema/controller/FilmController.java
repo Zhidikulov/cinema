0cbee7021b264c7219c7ff8783ed54e9656d1ba0
Asylbek Zhidikulov <zenit_777_94@mail.ru>
2022-07-21 22:38:06 +0500

    as

--- src/main/java/cinema/controller/FilmController.java
+++ src/main/java/cinema/controller/FilmController.java
@@ -3,14 +3,35 @@ package cinema.controller;
 import cinema.model.Film;
 import cinema.service.FilmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class FilmController {
 
     private final FilmService filmService;
 
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void addFilm(String keyword){
 
+        System.out.println(1);
+        String urlFilms = "https://kinopoiskapiunofficial.tech/api/v2.2/films/?keyword="+keyword;
+        String token = "fab0471f-1dfa-464b-8572-a78594f2ed5f";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("X-API-KEY", token);
+        HttpEntity<Film> req = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Film> response = restTemplate.exchange(urlFilms, HttpMethod.GET, req, Film.class);
+        Film film = response.getBody();
+        filmService.add(film);
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public void  addFilms(@ModelAttribute("keyword") String keyword){
+        addFilm(keyword);
+    }
 }
